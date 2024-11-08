267bf65e8a4c6fdd26139d53951d6cc04ddfd6a9
jacquikeating <j.keating@live.ca>
2024-11-07 22:18:15 -0500

    CORS & dotenv updates

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ app.get("/", (_req, res) => {
   res.send("Welcome to my API");
 });
 
-const corsOptions = {
-  origin: [
-    "https://warm-woodland-54729-516b83a873b7.herokuapp.com",
-    "https://venerable-praline-20eaaf.netlify.app",
-  ],
-  credentials: true,
-};
-
+// const corsOptions = {
+//   origin: [
+//     "https://warm-woodland-54729-516b83a873b7.herokuapp.com",
+//     "https://venerable-praline-20eaaf.netlify.app",
+//   ],
+//   credentials: true,
+// };
+app.use(cors());
 app.use("/warehouses", warehouseRoutes);
 app.use("/inventories", inventoryRoutes);
-// app.use(cors(*))
-app.use(cors(corsOptions));
+
+// app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
